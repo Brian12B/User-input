da33bad829c7039e39970bf95acc45335a8c8683
Brian12B <124532784+Brian12B@users.noreply.github.com>
2024-03-25 14:27:14 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+print('user information')
+name = input('what is your name? ')
+age = input('how old are your? ')
+location = input('where do you stay? ')
+print('Hello ' + name + ', you are ' + age + ' years old' ' and ' + 'live in ' + location)
+
